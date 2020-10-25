4a8c2fe2e9b47a116c39ecd6b3116e95d99dc8f5
Felix Gutierrez <fgutierrez2191@icloud.com>
2020-10-25 13:34:32 -0400

    figured out issue with array cycling

--- script.js
+++ script.js
@@ -1,45 +1,10 @@
-function buildQuiz(){
-    const output = [];
-
-    //for each question
-    myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-
-            //this will store the possible answers
-            const answers = [];
-            //for each available answer..
-            for(letter in currentQuestion.answers) {
-
-                //html radio button
-                answers.push(
-                    <label>
-                        <input type="radio" name="question${questionNumber}" value="${letter}">
-                        ${letter} :
-                        ${currentQuestion.answers[letter]}
-                    </label>
-                    
-                );
-            }
-        
-        output.push(
-            <div class="question"> ${currentQuestion.question} </div>
-            <div class="answers"> ${answers.join('')} </div>
-        );
-       }
-    );
-
-quizContainer.innerHTML = output.join('');
-
-}
-
-function showResults(){
-
-}
-
 //variables
+var questionIndex = 0;
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
+
+
 const myQuestions = [
     {
         question: "In what year was javascript invented?",
@@ -89,8 +54,92 @@ const myQuestions = [
 ];
 
 
+function buildQuiz(){
+    quizContainer.innerHTML = ""
+    
+    const output = [];
+    var currentQuestion = myQuestions[questionIndex]
+
+    //for each question
+    //myQuestions.forEach(
+        //(currentQuestion, questionNumber) => {
+
+            //this will store the possible answers
+            const answers = [];
+            //for each available answer..
+            for(letter in currentQuestion.answers) {
+
+                //html radio button
+                answers.push(
+                    `<label>
+                        <input type="radio" name="question${questionIndex}" value="${letter}">
+                        ${letter} :
+                        ${currentQuestion.answers[letter]}
+                    </label>`
+                    
+                );
+            }
+        
+        output.push(
+            `<div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join('')} </div>`
+        );
+       //}
+ //)
+
+quizContainer.innerHTML = output.join('')
+
+submitButton.addEventListener('click', showResults);
+}
+
+function showResults(){
+    console.log("click")
+//collect all answers from quiz
+var answerContainers = quizContainer.querySelectorAll('.answers');
+console.log(answerContainers)
+var currentQuestion = myQuestions[questionIndex]
+
+//keep track of users answers 
+let numCorrect = 0;
+
+//for each question
+//myQuestions.forEach( (currentQuestion, questionNumber) => 
+
+    //find selected answer
+    var answerContainer = answerContainers[0];
+    console.log(answerContainer)
+    var selector = `input[name=question${questionIndex}]:checked`;
+    console.log(selector);
+    var userAnswer = (answerContainer.querySelector(selector) || {}).value;
+    console.log(userAnswer);
+    // if answer is correct
+    if(userAnswer === currentQuestion.correctAnswer){
+        // add to the number of correct answers
+        numCorrect++;
+  
+        // color the answers green
+        answerContainers[0].style.color = 'lightgreen';
+      }
+      // if answer is wrong or blank
+      else{
+        // color the answers red
+        answerContainers[0].style.color = 'red';
+      }
+
+    questionIndex++;
+    buildQuiz(); 
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+
+
+
+
 //display quiz right away
 buildQuiz();
 
 //eventlistener
-submitButton.addEventListener('click', showResults);
+// submitButton.addEventListener('click', showResults);
+
